f32b738772273d9d6c58754e539d7197368ec477
Olga <luna33040@gmail.com>
2024-02-02 13:43:52 -0500

    Initial Commit

---
+++ eye9/eye9App.swift
@@ -0,0 +1,17 @@
+//
+//  eye9App.swift
+//  eye9
+//
+//  Created by Olga on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct eye9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
